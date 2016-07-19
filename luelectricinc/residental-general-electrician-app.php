516bac96517934044ae1a52e9386cee0ee0da55c
Sammy Roberts <omg.yeah.wai@hotmail.com>
2016-07-19 11:12:40 -0700

    typo and page fix

--- luelectricinc/residental-general-electrician-app.php
+++ luelectricinc/residental-general-electrician-app.php
@@ -1,69 +1,66 @@
 
-<div style="background-color: #E7E6E6">
 
 
-    <!-- Page Content -->
-    <div class="container">
-        <!-- Page Heading/Breadcrumbs -->
-        <p></p>
-        <div class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">Apply</h2>
-                <ol class="breadcrumb">
-                    <li><a href="index.html">Home</a>
-                    </li>
-                    <li>Careers</li>
-                    <li class="active">Apply</li>
-                </ol>
-            </div>
+<!-- Page Content -->
+<div class="container">
+    <!-- Page Heading/Breadcrumbs -->
+    <p></p>
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Apply</h2>
+            <ol class="breadcrumb">
+                <li><a href="index.html">Home</a>
+                </li>
+                <li>Careers</li>
+                <li class="active">Apply</li>
+            </ol>
         </div>
-
-        <div class="row">
-          <div class="col-lg-12">
-
-
-            <h1 class = "page-header">Residential/General Electrician <small>Full-Time <br>Orange County, CA – Los Angeles, CA – San Francisco, CA</small></h1>
-            <div class="panel panel-default">
-              <div class="panel-body">
-                <h5 class = "page-header">About L.U. ELECTRIC, INC.</h5>
-                <p>
-                  A family owned business operating for over 25 years, the goal of L.U. Electric, Inc.
-                  is to provide best in class service with the highest level of professionalism and integrity.
-                  We value the many talents and abilities of its employees, and is seeking an experienced General Electrician to
-                  join ongoing projects in the OC and LA areas. We are selective and careful when it comes to hiring. Plenty of
-                  room for advancement and professional development.
-                </p>
-                <h5 class = "page-header">Job Description</h5>
-                <p>
-                  We are seeking a Residential/General Electrician who is self-manageable,
-                  motivated and a reliable with great communication skills. A current Electrical
-                  Certification card is required. Candidates without electrical certification
-                  must be willing to test within 6 months of employment. Our current service
-                  work includes, but is not limited to residential and commercial electrical installations.
-                </p>
-                <h5 class="page-header">Prefferred Qualifications</h5>
-                <ul>
-                  <li>Knowledgeable of National Electric Code (NEC)</li>
-                  <li>Electrical Certification for the State of California or willing to test within 6 months of employment</li>
-                  <li>5-7+ years minimum General Electrician experience</li>
-                  <li>Clean driving record and valid State of California Driver’s license</li>
-                  <li>Ability to lift 75+ pounds</li>
-                  <li>Able to physically get into attics and crawl spaces</li>
-                  <li>Locations: OC, LA, Bay Area</li>
-                </ul>
-                <h5 class = "page-header">Additional information</h5>
-                  <p>L.U. Electric, Inc. is committed to hiring and retaining a
-                    diverse workforce. We are proud to be an Equal
-                    Opportunity/Affirmative Action Employer, making decisions
-                    without regard to race, color, religion, creed, sex, sexual
-                    orientation, gender identity, marital status, national origin,
-                    age, veteran status, disability, or any other protected class.
-                  <br><b>Please email resume to <a href="mailto:luelectric@luelectricinc.com">luelectric@luelectricinc.com.</a><b>
-                  </p>
-              </div>
-          </div>
-        </div>
-      </div>
-
-</div>
     </div>
+
+    <div class="row">
+      <div class="col-lg-12">
+
+
+        <h1 class = "page-header">Residential/General Electrician <small>Full-Time <br>Orange County, CA – Los Angeles, CA – San Francisco, CA</small></h1>
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <h5 class = "page-header">About L.U. ELECTRIC, INC.</h5>
+            <p>
+              A family owned business operating for over 25 years, the goal of L.U. Electric, Inc.
+              is to provide best in class service with the highest level of professionalism and integrity.
+              We value the many talents and abilities of our employees, and are seeking an experienced General Electrician to
+              join ongoing projects in the OC and LA areas. We are selective and careful when it comes to hiring. Plenty of
+              room for advancement and professional development.
+            </p>
+            <h5 class = "page-header">Job Description</h5>
+            <p>
+              We are seeking a Residential/General Electrician who is self-manageable,
+              motivated and a reliable with great communication skills. A current Electrical
+              Certification card is required. Candidates without electrical certification
+              must be willing to test within 6 months of employment. Our current service
+              work includes, but is not limited to residential and commercial electrical installations.
+            </p>
+            <h5 class="page-header">Prefferred Qualifications</h5>
+            <ul>
+              <li>Knowledgeable of National Electric Code (NEC)</li>
+              <li>Electrical Certification for the State of California or willing to test within 6 months of employment</li>
+              <li>5-7+ years minimum General Electrician experience</li>
+              <li>Clean driving record and valid State of California Driver’s license</li>
+              <li>Ability to lift 75+ pounds</li>
+              <li>Able to physically get into attics and crawl spaces</li>
+              <li>Locations: OC, LA, Bay Area</li>
+            </ul>
+            <h5 class = "page-header">Additional information</h5>
+              <p>L.U. Electric, Inc. is committed to hiring and retaining a
+                diverse workforce. We are proud to be an Equal
+                Opportunity/Affirmative Action Employer, making decisions
+                without regard to race, color, religion, creed, sex, sexual
+                orientation, gender identity, marital status, national origin,
+                age, veteran status, disability, or any other protected class.
+              <br><b>Please email resume to <a href="mailto:luelectric@luelectricinc.com">luelectric@luelectricinc.com.</a><b>
+              </p>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
