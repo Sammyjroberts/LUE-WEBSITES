881de330cba275a95fdc2bc4b0d2b0f9065a4c15
Sammy Roberts <omg.yeah.wai@hotmail.com>
2016-07-19 11:34:14 -0700

    removed nav and added apprentice page

--- luelectricinc/services.php
+++ luelectricinc/services.php
@@ -1,47 +1,4 @@
 
-<div style="background-color: #E7E6E6">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-custom navbar-fixed-top" role="navigation">
-        <div class="row topdesc" style='text-align: right; background-color: #E7E6E6; color: #454545; padding-right: 10%; white-space: nowrap; font-size: 12px'>17762 Mitchell N, Irvine, 92614 <span style="padding-left: 10pt"><span> <i class="fa fa-phone"></i>(949) 536-3200</span></span></div>
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand navbar-brandcustom" href="index.html"><img src="assets/LUELogoBig.png"></a>
-                <!--<a class="navbar-brand" href="index.html" style='font-size: 16pt;color: #CF000F'><strong>L.U. <span style="color:white;">Electric Inc.</span></strong></a> -->
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right" style="text-shadow:none;">
-                    <li>
-                        <a href="index.html">HOME</a>
-                    </li>
-                    <li class = "active">
-                        <a href="services.html" style="color: #E55E31">SERVICES</a>
-                    </li>
-
-                    <li>
-                        <a href="about.html">ABOUT US</a>
-                    </li>
-                    <li>
-                        <a href="contact.html">CONTACT</a>
-                    </li>
-                    <li>
-                        <a href="careers.html">CAREERS</a>
-                    </li>
-
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
     <!-- Page Content -->
     <div class="container">
@@ -300,4 +257,3 @@
         <!-- /.row -->
 
     </div>
-</div>
