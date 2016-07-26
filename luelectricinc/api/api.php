749e191d4918575271f54f82a65c48fe9068abdd
veleroth <brettkorp@gmail.com>
2016-07-26 10:45:24 -0700

    got rid of heades

--- luelectricinc/api/api.php
+++ luelectricinc/api/api.php
@@ -91,10 +91,10 @@
  *     "password" : "password"
  * }
  */
-
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Origin: *');
+//
+// header('Access-Control-Allow-Headers: Content-Type');
+// header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+// header('Access-Control-Allow-Origin: *');
 
 header('Content-Type: application/json');
 
